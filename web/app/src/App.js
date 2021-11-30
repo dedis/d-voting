8a5993a5f16df23f1a89227d5abc98ae3cc4d5be
Noémien Kocher <nkcr.je@gmail.com>
2021-11-30 16:16:45 +0100

    Updates the css to use tailwind
    Update the main layout to use the Tailwind library. From now on we will go with this library to implement the design. The "Create election" page has already been ported.

--- web/app/src/App.js
+++ web/app/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {useState} from 'react';
+import { useState } from 'react';
 import './App.css';
 import CreateElection from './components/election-creation/CreateElection';
 import Election from './components/election-status/Election';
@@ -11,57 +11,55 @@ import ResultTable from './components/result-page/ResultTable';
 import ResultPage from './components/result-page/ResultPage';
 import About from './components/about/About';
 import Footer from './components/footer/Footer';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import ElectionDetails from './components/election-status/ElectionDetails';
-import {LanguageContext} from './components/language/LanguageContext';
+import { LanguageContext } from './components/language/LanguageContext';
 import Login from './components/login/Login';
 import useToken from './components/utils/useToken';
 
 
 const App = () => {
-  
+
   const getBrowserLanguage = () => {
-    var userLang = navigator.userLanguage || navigator.language; 
-    if(userLang.substring(0,2) === 'fr'){
+    var userLang = navigator.userLanguage || navigator.language;
+    if (userLang.substring(0, 2) === 'fr') {
       return 'fr'
     }
     return 'en';
   }
 
   //language state
-  const [lanContext, setLanContext] =  useState(getBrowserLanguage());
-  const {token, saveToken} = useToken();
+  const [lanContext, setLanContext] = useState(getBrowserLanguage());
+  const { token, saveToken } = useToken();
 
   return (
-    <div className="App">
-      
-     <Router>
-        <LanguageContext.Provider value={[lanContext, setLanContext]}>         
+    <div className="App flex flex-col h-screen justify-between">
+
+      <Router>
+        <LanguageContext.Provider value={[lanContext, setLanContext]}>
           <div className='app-nav'>
-            <Route path='/:page' component={NavBar} />        
-            <Route exact path='/' component={NavBar}/>
+            <Route path='/:page' component={NavBar} />
+            <Route exact path='/' component={NavBar} />
           </div>
-          <div data-testid="content" className='app-page'>
-          {!token? (<div className='login-container'><Login id='login-id' setToken={saveToken}/></div>): (<div>
-            <Switch>
-              <Route path="/" exact component={Home}/>
-              <Route path="/create-election" component={CreateElection}/>
-              <Route path="/elections" exact component={Election}/>
-              <Route path="/elections/:id" component={ElectionDetails}/>
-              <Route path="/vote" exact component={BallotsTable}/>
-              <Route path="/results" exact component={ResultTable}/>
-              <Route path="/results/:id" exact component={ResultPage}/>
-              <Route path = "/vote/:id" component = {Ballot}/>
-              <Route path="/about" component={About}/>
-            </Switch>
+          <div data-testid="content" className='app-page mb-auto flex flex-row justify-center items-center w-full'>
+            {!token ? (<div className='login-container'><Login id='login-id' setToken={saveToken} /></div>) : (<div className="p-10 w-full max-w-screen-xl">
+              <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/create-election" component={CreateElection} />
+                <Route path="/elections" exact component={Election} />
+                <Route path="/elections/:id" component={ElectionDetails} />
+                <Route path="/vote" exact component={BallotsTable} />
+                <Route path="/results" exact component={ResultTable} />
+                <Route path="/results/:id" exact component={ResultPage} />
+                <Route path="/vote/:id" component={Ballot} />
+                <Route path="/about" component={About} />
+              </Switch>
             </div>)}
           </div>
-          <div className='footer-container'>
-            <Footer/>
-          </div>
+          <Footer />
         </LanguageContext.Provider>
-    </Router>
-  </div>
+      </Router>
+    </div>
   );
 }
 
