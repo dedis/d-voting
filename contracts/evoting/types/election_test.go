e357bb9d7a83ed109790ebfac46a8c36c5603c1f
Emilien <emilien.duc@epfl.ch>
2021-12-07 11:54:14 +0100

    adapt test to support configuration requirements

--- contracts/evoting/types/election_test.go
+++ contracts/evoting/types/election_test.go
@@ -5,22 +5,62 @@ import (
 	"testing"
 )
 
-var ballot1 = "select:aaa:true,false,true\n" +
+var ballot1 = "select:aaa:1,0,1\n" +
 	"rank:bbb:1,2,0,-128\n" +
 	"select:ddd:true,false,true,true,true\n" +
-	"text:ccc:blablablaf,cestmoiEmi"
+	"text:ccc:YmxhYmxhYmxhZg==,Y2VzdG1vaUVtaQ=="
 
-var ballot2 = "select:aaa:false,false,false\n" +
+var ballot2 = "select:aaa:0,0,0\n" +
 	"rank:bbb:-128,-128,-128,-128\n" +
-	"select:ddd:false,false,false,false,false\n" +
+	"select:ddd:0,0,0,0,0\n" +
 	"text:ccc:blablablaf,cestmoiEmi\n" +
 	"text:eee:aaaaaaaaaa,,"
 
 func TestBallot_Unmarshal(t *testing.T) {
 	b := Ballot{}
+	election := Election{BallotSize: len(ballot1)}
+	err := b.Unmarshal(ballot1, election)
 
-	err := b.Unmarshal(ballot1, Election{BallotSize: len(ballot1)})
+	require.EqualError(t, err, "wrong question ID: the question doesn't exist")
 
+	election.Configuration = Configuration{Scaffold: []Subject{{
+		Subjects: []Subject{},
+
+		Selects: []Select{{
+			ID:      "aaa",
+			Title:   "",
+			MaxN:    3,
+			MinN:    0,
+			Choices: make([]string, 3),
+		}, {
+			ID:      "ddd",
+			Title:   "",
+			MaxN:    5,
+			MinN:    0,
+			Choices: make([]string, 5),
+		}},
+
+		Ranks: []Rank{{
+			ID:      "bbb",
+			Title:   "",
+			MaxN:    4,
+			MinN:    0,
+			Choices: make([]string, 4),
+		}},
+
+		Texts: []Text{{
+			ID:        "ccc",
+			Title:     "",
+			MaxN:      2,
+			MinN:      0,
+			MaxLength: 10,
+			Regex:     "",
+			Choices:   make([]string, 2),
+		}},
+	},
+	}}
+
+	err = b.Unmarshal(ballot1, election)
 	require.NoError(t, err)
 
 	// expected ballot
@@ -100,3 +140,5 @@ func TestSubject_MaxEncodedSize(t *testing.T) {
 	require.Equal(t, len(ballot2), size)
 	require.Equal(t, subject.MaxEncodedSize(), size)
 }
+
+//TODO Test configuration requirements
