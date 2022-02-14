1ae67e6a13c3e0b389e6b043a46be8f736e284a7
Jean Viaene <jean.viaene@epfl.ch>
2022-02-14 11:36:54 +0100

    fixed NavBar

--- web/frontend/src/components/NavBar.tsx
+++ web/frontend/src/components/NavBar.tsx
@@ -230,33 +230,37 @@ const NavBar: FC<NavBarProps> = ({ lastname, firstname, sciper, role }) => {
                 aria-labelledby="user-menu-button"
                 tabIndex={-1}
               >
-                {/* <a
-                  href="#"
-                  className="block px-4 py-2 text-sm text-gray-700"
-                  role="menuitem"
-                  tabIndex={-1}
-                  id="user-menu-item-0"
-                >
-                  Your Profile
-                </a>
-                <a
-                  href="#"
-                  className="block px-4 py-2 text-sm text-gray-700"
-                  role="menuitem"
-                  tabIndex={-1}
-                  id="user-menu-item-1"
-                >
-                  Settings
-                </a>
-                <a
-                  href="#"
-                  className="block px-4 py-2 text-sm text-gray-700"
-                  role="menuitem"
-                  tabIndex={-1}
-                  id="user-menu-item-2"
-                >
-                  Sign out
-                </a> */}
+                {
+                  <div>
+                    <a
+                      href="#top"
+                      className="block px-4 py-2 text-sm text-gray-700"
+                      role="menuitem"
+                      tabIndex={-1}
+                      id="user-menu-item-0"
+                    >
+                      Your Profile
+                    </a>
+                    <a
+                      href="#top"
+                      className="block px-4 py-2 text-sm text-gray-700"
+                      role="menuitem"
+                      tabIndex={-1}
+                      id="user-menu-item-1"
+                    >
+                      Settings
+                    </a>
+                    <a
+                      href="#top"
+                      className="block px-4 py-2 text-sm text-gray-700"
+                      role="menuitem"
+                      tabIndex={-1}
+                      id="user-menu-item-2"
+                    >
+                      Sign out
+                    </a>
+                  </div>
+                }
               </div>
             </div>
           </div>
