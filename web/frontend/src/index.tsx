b267102624116c242f19ed14a8396a45923d1466
Noémien Kocher <nkcr.je@gmail.com>
2022-02-23 08:53:20 +0100

    Updates minor comments

--- web/frontend/src/index.tsx
+++ web/frontend/src/index.tsx
@@ -13,7 +13,7 @@ import './index.css';
 import App from './layout/App';
 import reportWebVitals from './reportWebVitals';
 
-// Be default we load the mock messages when not in production. This is handy
+// By default we load the mock messages when not in production. This is handy
 // because it removes the need to have a backend server.
 if (process.env.NODE_ENV !== 'production' && process.env.REACT_APP_NOMOCK !== 'on') {
   const { dvotingserver } = require('./mocks/dvotingserver');
@@ -25,7 +25,7 @@ const defaultAuth = { isLogged: false, firstname: '', lastname: '', role: '' };
 // AuthContext is a global state containing the authentication state. React
 // Context is a way to manage state globally, without having to pass props to
 // components. This context is set when loading the app by asking the backend if
-// the use is logged.
+// the user is logged.
 export const AuthContext = createContext<AuthState>(defaultAuth);
 
 export interface AuthState {
@@ -35,7 +35,8 @@ export interface AuthState {
   role: string;
 }
 
-// A small elements to display that the page is loading
+// A small elements to display that the page is loading, should be something
+// more elegant in the future and be its own component.
 const Loading: FC = () => <p>App is loading...</p>;
 
 // AppContainer wraps the App with the context. It makes sure that the App is
