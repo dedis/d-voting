b211a375fb9690b0628adcd30a7d83dcf7294402
Guanyu Zhang <51890833+JBGYZ@users.noreply.github.com>
2022-04-27 12:43:18 +0200

    Jinja template for docker-compose file
    Used jinja template to generate docker-compose file for multiple nodes start

--- setup_docker.sh
+++ setup_docker.sh
@@ -9,43 +9,54 @@ set -e
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-# rm -rf ./nodedata    
-# mkdir nodedata
+N_NODE=$1
+vals=($(seq 2 1 $N_NODE))
 
+# for i in "${vals[@]}"
+# do
+#     A=$((i+200))
+#     echo $A
+# done
 
 echo "${GREEN}[1/7]${NC} connect nodes"
 conn_token=$(docker exec node1 memcoin --config /tmp/node1 minogrpc token)
-eval docker exec node2 memcoin --config /tmp/node2 minogrpc join \
+
+vals=($(seq 2 1 $N_NODE))
+
+for i in "${vals[@]}"
+do
+    eval docker exec node$i memcoin --config /tmp/node$i minogrpc join \
     --address 127.0.0.1:2001 $conn_token
-eval docker exec node3 memcoin --config /tmp/node3 minogrpc join \
-    --address 127.0.0.1:2001 $conn_token
+done
+
 
 
 echo "${GREEN}[2/7]${NC} create a chain"
-join1=$(docker exec node1 memcoin --config /tmp/node1 ordering export)
-join2=$(docker exec node2 memcoin --config /tmp/node2 ordering export)
-join3=$(docker exec node3 memcoin --config /tmp/node3 ordering export)
+vals=($(seq 1 1 $N_NODE))
+ARRAY=()
+for i in "${vals[@]}"
+do
+    ARRAY+=($(docker exec node$i memcoin --config /tmp/node$i ordering export))
+done
+
 
 docker exec node1 memcoin --config /tmp/node1 ordering setup\
-    --member $join1\
-    --member $join2\
-    --member $join3
+    --member $ARRAY
 
 
 echo "${GREEN}[3/7]${NC} setup access rights on each node"
 access_token=$(docker exec node1 crypto bls signer read --path private.key --format BASE64_PUBKEY)
-docker exec node1 memcoin --config /tmp/node1 access add \
+
+for i in "${vals[@]}"
+do
+    docker exec node$i memcoin --config /tmp/node$i access add \
     --identity $access_token
-docker exec node2 memcoin --config /tmp/node2 access add \
-    --identity $access_token
-docker exec node3 memcoin --config /tmp/node3 access add \
-    --identity $access_token
+done
+
 
 
 echo "${GREEN}[4/7]${NC} grant access on the chain"
-access_token1=$(docker exec node1 crypto bls signer read --path /tmp/node1/private.key --format BASE64_PUBKEY)
-access_token2=$(docker exec node2 crypto bls signer read --path /tmp/node2/private.key --format BASE64_PUBKEY)
-access_token3=$(docker exec node3 crypto bls signer read --path /tmp/node3/private.key --format BASE64_PUBKEY)
+
 
 docker exec node1 memcoin --config /tmp/node1 pool add\
     --key private.key\
@@ -56,50 +67,36 @@ docker exec node1 memcoin --config /tmp/node1 pool add\
     --args access:identity --args $access_token\
     --args access:command --args GRANT
 
-docker exec node1 memcoin --config /tmp/node1 pool add\
-    --key private.key\
-    --args go.dedis.ch/dela.ContractArg --args go.dedis.ch/dela.Access\
-    --args access:grant_id --args 0300000000000000000000000000000000000000000000000000000000000000\
-    --args access:grant_contract --args go.dedis.ch/dela.Evoting\
-    --args access:grant_command --args all\
-    --args access:identity --args $access_token1\
-    --args access:command --args GRANT
+for i in "${vals[@]}"
+do
+    access_token_tmp=$(docker exec node$i crypto bls signer read --path /tmp/node$i/private.key --format BASE64_PUBKEY)
+    docker exec node1 memcoin --config /tmp/node1 pool add\
+        --key private.key\
+        --args go.dedis.ch/dela.ContractArg --args go.dedis.ch/dela.Access\
+        --args access:grant_id --args 0300000000000000000000000000000000000000000000000000000000000000\
+        --args access:grant_contract --args go.dedis.ch/dela.Evoting\
+        --args access:grant_command --args all\
+        --args access:identity --args $access_token_tmp\
+        --args access:command --args GRANT
+done
 
-docker exec node1 memcoin --config /tmp/node1 pool add\
-    --key private.key\
-    --args go.dedis.ch/dela.ContractArg --args go.dedis.ch/dela.Access\
-    --args access:grant_id --args 0300000000000000000000000000000000000000000000000000000000000000\
-    --args access:grant_contract --args go.dedis.ch/dela.Evoting\
-    --args access:grant_command --args all\
-    --args access:identity --args $access_token2\
-    --args access:command --args GRANT    
 
-docker exec node1 memcoin --config /tmp/node1 pool add\
-    --key private.key\
-    --args go.dedis.ch/dela.ContractArg --args go.dedis.ch/dela.Access\
-    --args access:grant_id --args 0300000000000000000000000000000000000000000000000000000000000000\
-    --args access:grant_contract --args go.dedis.ch/dela.Evoting\
-    --args access:grant_command --args all\
-    --args access:identity --args $access_token3\
-    --args access:command --args GRANT
 
 echo "${GREEN}[5/7]${NC} init shuffle"
-docker exec node1 memcoin --config /tmp/node1 shuffle init --signer /tmp/node1/private.key
-docker exec node2 memcoin --config /tmp/node2 shuffle init --signer /tmp/node2/private.key
-docker exec node3 memcoin --config /tmp/node3 shuffle init --signer /tmp/node3/private.key
+for i in "${vals[@]}"
+do
+    docker exec node$i memcoin --config /tmp/node$i shuffle init --signer /tmp/node$i/private.key
+done
 
 echo "${GREEN}[6/7]${NC} starting http proxy"
-docker exec node1 memcoin --config /tmp/node1 proxy start --clientaddr 127.0.0.1:8081
-docker exec node1 memcoin --config /tmp/node1 e-voting registerHandlers --signer private.key
-docker exec node1 memcoin --config /tmp/node1 dkg registerHandlers
+for i in "${vals[@]}"
+do
+    docker exec node$i memcoin --config /tmp/node$i proxy start --clientaddr 127.0.0.1:$((i+8080))
+    docker exec node$i memcoin --config /tmp/node$i e-voting registerHandlers --signer private.key
+    docker exec node$i memcoin --config /tmp/node$i dkg registerHandlers
+done
 
-docker exec node2 memcoin --config /tmp/node2 proxy start --clientaddr 127.0.0.1:8082
-docker exec node2 memcoin --config /tmp/node2 e-voting registerHandlers --signer private.key
-docker exec node2 memcoin --config /tmp/node2 dkg registerHandlers
 
-docker exec node3 memcoin --config /tmp/node3 proxy start --clientaddr 127.0.0.1:8083
-docker exec node3 memcoin --config /tmp/node3 e-voting registerHandlers --signer private.key
-docker exec node3 memcoin --config /tmp/node3 dkg registerHandlers
 
 # If an election is created with ID "deadbeef" then one must set up DKG
 # on each node before the election can proceed:
