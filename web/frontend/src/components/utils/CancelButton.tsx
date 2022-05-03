4ed60d0c86fe576f3f971fb029b5b5ac66dbbad9
badrlarhdir <badrlarhdir7@gmail.com>
2022-05-03 09:21:21 +0200

    Adds a spinner when the App is loading

--- web/frontend/src/components/utils/CancelButton.tsx
+++ web/frontend/src/components/utils/CancelButton.tsx
@@ -14,7 +14,7 @@ const CancelButton = ({ status, handleCancel }) => {
     isAuthorized &&
     status === STATUS.Open && (
       <button onClick={handleCancel}>
-        <div className="whitespace-nowrap inline-flex items-center justify-center px-4 py-1 border border-gray-300 text-sm rounded-full font-medium text-gray-700">
+        <div className="whitespace-nowrap inline-flex items-center justify-center px-4 py-1 mr-2 border border-gray-300 text-sm rounded-full font-medium text-gray-700">
           <XIcon className="-ml-1 mr-2 h-5 w-5 text-gray-700" aria-hidden="true" />
           {t('cancel')}
         </div>
--- web/frontend/src/components/utils/CloseButton.tsx
+++ web/frontend/src/components/utils/CloseButton.tsx
@@ -15,7 +15,7 @@ const CloseButton = ({ status, handleClose }) => {
     isAuthorized &&
     status === STATUS.Open && (
       <button onClick={handleClose}>
-        <div className="whitespace-nowrap inline-flex items-center justify-center px-4 py-1 border border-gray-300 text-sm rounded-full font-medium text-gray-700">
+        <div className="whitespace-nowrap inline-flex items-center justify-center px-4 py-1 mr-2 border border-gray-300 text-sm rounded-full font-medium text-gray-700">
           <LockClosedIcon className="-ml-1 mr-2 h-5 w-5 text-gray-700" aria-hidden="true" />
           {t('close')}
         </div>
