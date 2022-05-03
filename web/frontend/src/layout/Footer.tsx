706930a84cdf25eb8174accefef9d3a6fe4e8e21
badrlarhdir <badrlarhdir7@gmail.com>
2022-05-03 09:45:53 +0200

    Fixes background footer

--- web/frontend/src/layout/Footer.tsx
+++ web/frontend/src/layout/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => (
-  <div className="flex flex-row border-t justify-center items-center w-full p-4 text-gray-300 text-xs">
-    <footer className="bg-white">
+  <div className="flex flex-row border-t justify-center bg-white items-center w-full p-4 text-gray-300 text-xs">
+    <footer>
       <div className="max-w-7xl mx-auto py-2 px-4 overflow-hidden sm:px-6 lg:px-8">
         <span className="text-gray-400"> &copy; 2022 DEDIS LAB - </span>
         <a className="text-gray-600" href="https://github.com/dedis/dela">
