65c24f1ca11dfa637f7d5208b3bd396fb52b65ce
Guanyu Zhang <51890833+JBGYZ@users.noreply.github.com>
2022-05-17 11:03:19 +0200

    automatised tests in a sequential way

---
+++ autotest.sh
@@ -0,0 +1,21 @@
+#! /bin/sh
+
+RUN_TIMES=5
+N_NODE=15
+
+vals=($(seq 1 1 $RUN_TIMES))
+
+
+for i in "${vals[@]}"
+do
+    echo "Test $i with $N_NODE nodes"
+    ./launch_containers.sh $N_NODE
+    sleep 3
+    ./setup_containers.sh $N_NODE
+    sleep 3
+    DVOTING_NB_NODE=$N_NODE go test -v -run ^TestScenario$ github.com/dedis/d-voting/integration -count=1 | tee ./log/gotest.log
+    sleep 3
+    ./kill_test.sh
+    mv log logkill$i
+    mkdir log
+done
