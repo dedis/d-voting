b01b83ab2ccf292fa933aef49fa97748e5dfe2ff
Guanyu Zhang <51890833+JBGYZ@users.noreply.github.com>
2022-05-19 11:50:09 +0200

    Added comments on autotest.sh

--- autotest.sh
+++ autotest.sh
@@ -1,6 +1,11 @@
 #! /bin/sh
 
+# This script uses launch_containers.sh, setup_containers.sh, kill_test.sh to launch multiple 
+# times of scenario test with user defined number of nodes. The results of log are kept in directory named logkill$i
+
+# Set how many times we want to run the test
 RUN_TIMES=15
+# Set the number of nodes in the test, currently this number is fixed for all tests
 N_NODE=15
 vals=($(seq 1 1 $RUN_TIMES))
 
@@ -8,13 +13,18 @@ vals=($(seq 1 1 $RUN_TIMES))
 for i in "${vals[@]}"
 do
     echo "Test $i with $N_NODE nodes"
+    # Launch nodes
     ./launch_containers.sh $N_NODE false
     sleep 3
+    # Setup block chain
     ./setup_containers.sh $N_NODE
     sleep 3
-    DVOTING_NB_NODE=$N_NODE go test -v -run ^TestScenario$ github.com/dedis/d-voting/integration -count=1 | tee ./log/gotest.log
+    # Start scenario test and keep logs 
+    NNODES=$N_NODE go test -v -run ^TestScenario$ github.com/dedis/d-voting/integration -count=1 | tee ./log/gotest.log
     sleep 3
+    # Stop the test
     ./kill_test.sh
+    # move log to a new directory named logkill
     mv log logkill$i
     mkdir log
 done
