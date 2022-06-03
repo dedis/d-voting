c693e3c65864a27059e2487a51d441c3ea109e83
cmsigrist <capucine.berger@epfl.ch>
2022-06-03 11:28:51 +0200

    Remove useless hook useAnswer

--- web/frontend/src/components/utils/useConfiguration.tsx
+++ web/frontend/src/components/utils/useConfiguration.tsx
@@ -1,11 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Answers, Configuration } from 'types/configuration';
-import { emptyConfiguration, newAnswer } from 'types/getObjectType';
-import {
-  unmarshalConfig,
-  unmarshalConfigAndCreateAnswers,
-  unmarshalSubjectAndCreateAnswers,
-} from 'types/JSONparser';
+import { emptyConfiguration } from 'types/getObjectType';
+import { unmarshalConfig, unmarshalConfigAndCreateAnswers } from 'types/JSONparser';
 
 // Returns a Configuration and the initialized Answers
 const useConfiguration = (configObj: any) => {
@@ -40,23 +36,4 @@ const useConfigurationOnly = (configObj: any) => {
   return configuration;
 };
 
-// Custom hook to create answers from a Configuration.
-const useAnswers = (configuration: Configuration) => {
-  const [answers, setAnswers] = useState<Answers>(null);
-
-  useEffect(() => {
-    if (configuration !== null) {
-      const newAnswers: Answers = newAnswer();
-
-      for (const subjectObj of configuration.Scaffold) {
-        unmarshalSubjectAndCreateAnswers(subjectObj, newAnswers);
-      }
-
-      setAnswers(newAnswers);
-    }
-  }, [configuration]);
-
-  return { answers, setAnswers };
-};
-
-export { useConfiguration, useConfigurationOnly, useAnswers };
+export { useConfiguration, useConfigurationOnly };
