58f2ccf94233a5e4d217e4f125c247f47e83f7bc
Ahmed Kamal Elalamy <ahmed.elalamy@epfl.ch>
2022-11-23 10:49:49 +0100

    Refactor code to have the least duplication possible

--- web/frontend/src/pages/form/GroupedResult.tsx
+++ web/frontend/src/pages/form/GroupedResult.tsx
@@ -1,6 +1,5 @@
-import React, { FC, useEffect, useState } from 'react';
-import PropTypes from 'prop-types';
-import { DownloadedResults, RankResults, SelectResults, TextResults } from 'types/form';
+import { FC } from 'react';
+import { RankResults, SelectResults, TextResults } from 'types/form';
 import SelectResult from './components/SelectResult';
 import RankResult from './components/RankResult';
 import TextResult from './components/TextResult';
@@ -15,158 +14,22 @@ import {
   SubjectElement,
   TEXT,
 } from 'types/configuration';
-import DownloadButton from 'components/buttons/DownloadButton';
-import { useTranslation } from 'react-i18next';
-import saveAs from 'file-saver';
 import { useParams } from 'react-router-dom';
-import { useNavigate } from 'react-router';
 import useForm from 'components/utils/useForm';
 import { useConfigurationOnly } from 'components/utils/useConfiguration';
-import {
-  countRankResult,
-  countSelectResult,
-  countTextResult,
-} from './components/utils/countResult';
+
+type GroupedResultProps = {
+  rankResult: RankResults;
+  selectResult: SelectResults;
+  textResult: TextResults;
+};
 
 // Functional component that displays the result of the votes
-const GroupedResult: FC = () => {
-  const { t } = useTranslation();
-  const navigate = useNavigate();
+const GroupedResult: FC<GroupedResultProps> = ({ rankResult, selectResult, textResult }) => {
   const { formId } = useParams();
 
-  const { loading, result, configObj } = useForm(formId);
+  const { result, configObj } = useForm(formId);
   const configuration = useConfigurationOnly(configObj);
-
-  const [rankResult, setRankResult] = useState<RankResults>(null);
-  const [selectResult, setSelectResult] = useState<SelectResults>(null);
-  const [textResult, setTextResult] = useState<TextResults>(null);
-
-  // Group the different results by the ID of the question,
-  const groupByID = (
-    resultMap: Map<ID, number[][] | string[][]>,
-    IDs: ID[],
-    results: boolean[][] | number[][] | string[][],
-    toNumber: boolean = false
-  ) => {
-    IDs.forEach((id, index) => {
-      let updatedRes = [];
-      let res = results[index];
-
-      // SelectResult are mapped to 0 or 1s, such that ballots can be
-      // counted more efficiently
-      if (toNumber) {
-        res = (results[index] as boolean[]).map((r: boolean) => (r ? 1 : 0));
-      }
-
-      if (resultMap.has(id)) {
-        updatedRes = resultMap.get(id);
-      }
-
-      updatedRes.push(res);
-      resultMap.set(id, updatedRes);
-    });
-  };
-
-  const groupResultsByID = () => {
-    let selectRes: SelectResults = new Map<ID, number[][]>();
-    let rankRes: RankResults = new Map<ID, number[][]>();
-    let textRes: TextResults = new Map<ID, string[][]>();
-
-    result.forEach((res) => {
-      if (
-        res.SelectResultIDs !== null &&
-        res.RankResultIDs !== null &&
-        res.TextResultIDs !== null
-      ) {
-        groupByID(selectRes, res.SelectResultIDs, res.SelectResult, true);
-        groupByID(rankRes, res.RankResultIDs, res.RankResult);
-        groupByID(textRes, res.TextResultIDs, res.TextResult);
-      }
-    });
-
-    return { rankRes, selectRes, textRes };
-  };
-
-  useEffect(() => {
-    if (result !== null) {
-      const { rankRes, selectRes, textRes } = groupResultsByID();
-
-      setRankResult(rankRes);
-      setSelectResult(selectRes);
-      setTextResult(textRes);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [result]);
-
-  const getResultData = (subject: Subject, dataToDownload: DownloadedResults[]) => {
-    dataToDownload.push({ Title: subject.Title });
-
-    subject.Order.forEach((id: ID) => {
-      const element = subject.Elements.get(id);
-      let res = undefined;
-
-      switch (element.Type) {
-        case RANK:
-          const rank = element as RankQuestion;
-
-          if (rankResult.has(id)) {
-            res = countRankResult(rankResult.get(id), element as RankQuestion).resultsInPercent.map(
-              (percent, index) => {
-                return { Candidate: rank.Choices[index], Percentage: `${percent}%` };
-              }
-            );
-            dataToDownload.push({ Title: element.Title, Results: res });
-          }
-          break;
-
-        case SELECT:
-          const select = element as SelectQuestion;
-
-          if (selectResult.has(id)) {
-            res = countSelectResult(selectResult.get(id)).resultsInPercent.map((percent, index) => {
-              return { Candidate: select.Choices[index], Percentage: `${percent}%` };
-            });
-            dataToDownload.push({ Title: element.Title, Results: res });
-          }
-          break;
-
-        case SUBJECT:
-          getResultData(element as Subject, dataToDownload);
-          break;
-
-        case TEXT:
-          if (textResult.has(id)) {
-            res = Array.from(countTextResult(textResult.get(id)).resultsInPercent).map((r) => {
-              return { Candidate: r[0], Percentage: `${r[1]}%` };
-            });
-            dataToDownload.push({ Title: element.Title, Results: res });
-          }
-          break;
-      }
-    });
-  };
-
-  const exportJSONData = () => {
-    const fileName = 'result.json';
-
-    const dataToDownload: DownloadedResults[] = [];
-
-    configuration.Scaffold.forEach((subject: Subject) => {
-      getResultData(subject, dataToDownload);
-    });
-
-    const data = {
-      Title: configuration.MainTitle,
-      NumberOfVotes: result.length,
-      Results: dataToDownload,
-    };
-
-    const fileToSave = new Blob([JSON.stringify(data, null, 2)], {
-      type: 'application/json',
-    });
-
-    saveAs(fileToSave, fileName);
-  };
 
   const SubjectElementResultDisplay = (element: SubjectElement) => {
     return (
@@ -189,7 +52,7 @@ const GroupedResult: FC = () => {
   };
 
   const displayResults = (subject: Subject) => {
-    console.log(rankResult);
+    console.log(result);
     return (
       <div key={subject.ID}>
         <h2 className="text-xl pt-1 pb-1 sm:pt-2 sm:pb-2 border-t font-bold text-gray-600">
@@ -215,22 +78,9 @@ const GroupedResult: FC = () => {
       <div className="flex flex-col">
         {configuration.Scaffold.map((subject: Subject) => displayResults(subject))}
       </div>
-      <div className="flex my-4">
-        <button
-          type="button"
-          onClick={() => navigate(-1)}
-          className="text-gray-700 my-2 mr-2 items-center px-4 py-2 border rounded-md text-sm hover:text-indigo-500">
-          {t('back')}
-        </button>
-
-        <DownloadButton exportData={exportJSONData}>{t('exportJSON')}</DownloadButton>
-      </div>
+      <div className="flex my-4"></div>
     </div>
   );
-};
-
-GroupedResult.propTypes = {
-  location: PropTypes.any,
 };
 
 export default GroupedResult;
--- web/frontend/src/pages/form/Result.tsx
+++ web/frontend/src/pages/form/Result.tsx
@@ -1,26 +1,174 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-
 import { useTranslation } from 'react-i18next';
-
+import { DownloadedResults, RankResults, SelectResults, TextResults } from 'types/form';
+import {
+  ID,
+  RANK,
+  RankQuestion,
+  SELECT,
+  SUBJECT,
+  SelectQuestion,
+  Subject,
+  TEXT,
+} from 'types/configuration';
 import { useParams } from 'react-router-dom';
-
+import { useNavigate } from 'react-router';
 import useForm from 'components/utils/useForm';
 import { useConfigurationOnly } from 'components/utils/useConfiguration';
-
+import DownloadButton from 'components/buttons/DownloadButton';
 import Loading from 'pages/Loading';
+import saveAs from 'file-saver';
 import ResultExplanation from './components/ResultExplanation';
 import { Tab } from '@headlessui/react';
 import GroupedResult from './GroupedResult';
 import IndividualResult from './IndividualResult';
+import {
+  countRankResult,
+  countSelectResult,
+  countTextResult,
+} from './components/utils/countResult';
 
 // Functional component that displays the result of the votes
 const FormResult: FC = () => {
   const { t } = useTranslation();
+  const navigate = useNavigate();
   const { formId } = useParams();
 
   const { loading, result, configObj } = useForm(formId);
   const configuration = useConfigurationOnly(configObj);
+
+  const [rankResult, setRankResult] = useState<RankResults>(null);
+  const [selectResult, setSelectResult] = useState<SelectResults>(null);
+  const [textResult, setTextResult] = useState<TextResults>(null);
+
+  const getResultData = (subject: Subject, dataToDownload: DownloadedResults[]) => {
+    dataToDownload.push({ Title: subject.Title });
+
+    subject.Order.forEach((id: ID) => {
+      const element = subject.Elements.get(id);
+      let res = undefined;
+
+      switch (element.Type) {
+        case RANK:
+          const rank = element as RankQuestion;
+
+          if (rankResult.has(id)) {
+            res = countRankResult(rankResult.get(id), element as RankQuestion).resultsInPercent.map(
+              (percent, index) => {
+                return { Candidate: rank.Choices[index], Percentage: `${percent}%` };
+              }
+            );
+            dataToDownload.push({ Title: element.Title, Results: res });
+          }
+          break;
+
+        case SELECT:
+          const select = element as SelectQuestion;
+
+          if (selectResult.has(id)) {
+            res = countSelectResult(selectResult.get(id)).resultsInPercent.map((percent, index) => {
+              return { Candidate: select.Choices[index], Percentage: `${percent}%` };
+            });
+            dataToDownload.push({ Title: element.Title, Results: res });
+          }
+          break;
+
+        case SUBJECT:
+          getResultData(element as Subject, dataToDownload);
+          break;
+
+        case TEXT:
+          if (textResult.has(id)) {
+            res = Array.from(countTextResult(textResult.get(id)).resultsInPercent).map((r) => {
+              return { Candidate: r[0], Percentage: `${r[1]}%` };
+            });
+            dataToDownload.push({ Title: element.Title, Results: res });
+          }
+          break;
+      }
+    });
+  };
+
+  // Group the different results by the ID of the question,
+  const groupByID = (
+    resultMap: Map<ID, number[][] | string[][]>,
+    IDs: ID[],
+    results: boolean[][] | number[][] | string[][],
+    toNumber: boolean = false
+  ) => {
+    IDs.forEach((id, index) => {
+      let updatedRes = [];
+      let res = results[index];
+
+      // SelectResult are mapped to 0 or 1s, such that ballots can be
+      // counted more efficiently
+      if (toNumber) {
+        res = (results[index] as boolean[]).map((r: boolean) => (r ? 1 : 0));
+      }
+
+      if (resultMap.has(id)) {
+        updatedRes = resultMap.get(id);
+      }
+
+      updatedRes.push(res);
+      resultMap.set(id, updatedRes);
+    });
+  };
+
+  const groupResultsByID = () => {
+    let selectRes: SelectResults = new Map<ID, number[][]>();
+    let rankRes: RankResults = new Map<ID, number[][]>();
+    let textRes: TextResults = new Map<ID, string[][]>();
+
+    result.forEach((res) => {
+      if (
+        res.SelectResultIDs !== null &&
+        res.RankResultIDs !== null &&
+        res.TextResultIDs !== null
+      ) {
+        groupByID(selectRes, res.SelectResultIDs, res.SelectResult, true);
+        groupByID(rankRes, res.RankResultIDs, res.RankResult);
+        groupByID(textRes, res.TextResultIDs, res.TextResult);
+      }
+    });
+
+    return { rankRes, selectRes, textRes };
+  };
+
+  useEffect(() => {
+    if (result !== null) {
+      const { rankRes, selectRes, textRes } = groupResultsByID();
+
+      setRankResult(rankRes);
+      setSelectResult(selectRes);
+      setTextResult(textRes);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [result]);
+
+  const exportJSONData = () => {
+    const fileName = 'result.json';
+
+    const dataToDownload: DownloadedResults[] = [];
+
+    configuration.Scaffold.forEach((subject: Subject) => {
+      getResultData(subject, dataToDownload);
+    });
+
+    const data = {
+      Title: configuration.MainTitle,
+      NumberOfVotes: result.length,
+      Results: dataToDownload,
+    };
+
+    const fileToSave = new Blob([JSON.stringify(data, null, 2)], {
+      type: 'application/json',
+    });
+
+    saveAs(fileToSave, fileName);
+  };
+
   return (
     <div className="w-[60rem] font-sans px-4 pt-8 pb-4">
       {!loading ? (
@@ -64,13 +212,31 @@ const FormResult: FC = () => {
                   </Tab>
                 </Tab.List>
                 <Tab.Panel>
-                  <GroupedResult />
+                  <GroupedResult
+                    rankResult={rankResult}
+                    selectResult={selectResult}
+                    textResult={textResult}
+                  />
                 </Tab.Panel>
                 <Tab.Panel>
-                  <IndividualResult />
+                  <IndividualResult
+                    rankResult={rankResult}
+                    selectResult={selectResult}
+                    textResult={textResult}
+                  />
                 </Tab.Panel>
               </Tab.Group>
             </div>
+          </div>
+          <div className="flex my-4">
+            <button
+              type="button"
+              onClick={() => navigate(-1)}
+              className="text-gray-700 my-2 mr-2 items-center px-4 py-2 border rounded-md text-sm hover:text-indigo-500">
+              {t('back')}
+            </button>
+
+            <DownloadButton exportData={exportJSONData}>{t('exportJSON')}</DownloadButton>
           </div>
         </div>
       ) : (
