8b56fe446b7a47def9f63206dfe0c4dce5eefd6f
mamine2207 <mohamed.benaziz@epfl.ch>
2022-12-13 23:50:18 +0100

    NewFormVote() edited

--- proxy/election.go
+++ proxy/election.go
@@ -166,7 +166,7 @@ func (h *form) NewFormVote(w http.ResponseWriter, r *http.Request) {
 
 	ciphervote := make(types.Ciphervote, len(req.Ballot))
 
-	// encrypt the vote 
+	// Unmarshalling the ballots and keys from the request.
 	for i, egpair := range req.Ballot {
 		k := suite.Point()
 
