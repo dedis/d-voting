a79abfd0ccc534688f890eb32ef0f52af2c9d19f
A <albert.troussard@epfl.ch>
2022-12-14 10:44:25 +0100

    import fix

--- proxy/mod.go
+++ proxy/mod.go
@@ -14,7 +14,6 @@ import (
 
 	"github.com/dedis/d-voting/contracts/evoting"
 	"github.com/dedis/d-voting/proxy/types"
-	ptypes "github.com/dedis/d-voting/proxy/types"
 	"go.dedis.ch/kyber/v3/suites"
 )
 
@@ -36,12 +35,13 @@ type Form interface {
 	DeleteForm(http.ResponseWriter, *http.Request)
 }
 
+// Transaction defines the public HTTP API of the transaction verification service
 type Transaction interface {
 	// GET /transactions/{token}
 	IsTxnIncluded(http.ResponseWriter, *http.Request)
 	submitTxn(ctx context.Context, cmd evoting.Command, cmdArg string, payload []byte) ([]byte, uint64, error)
-	CreateTransactionInfoToSend(txnID []byte, lastBlockIdx uint64, status ptypes.TransactionStatus) (ptypes.TransactionInfoToSend, error)
-	sendTransactionInfo(w http.ResponseWriter, txnID []byte, lastBlockIdx uint64, status ptypes.TransactionStatus) error
+	CreateTransactionInfoToSend(txnID []byte, lastBlockIdx uint64, status types.TransactionStatus) (types.TransactionInfoToSend, error)
+	sendTransactionInfo(w http.ResponseWriter, txnID []byte, lastBlockIdx uint64, status types.TransactionStatus) error
 
 }
 
