cadaa39905bcffdedcebbe68bbcb8dd0c9502175
Khadija Tagemouati <khadija.tagemouati@epfl.ch>
2022-12-22 11:39:32 +0100

    Resolves linting errors in WarningModal.tsx

--- web/frontend/src/layout/components/WarningModal.tsx
+++ web/frontend/src/layout/components/WarningModal.tsx
@@ -9,48 +9,44 @@ type WarningModalProps = {
   action: () => void;
 };
 
-const WarningModal : FC<WarningModalProps> = ({message,isShown,setIsShown,action}) => {
-    const { t } = useTranslation();
-    if(isShown) {
-        return (
-            <Dialog open={isShown} onClose={() => {}}>
-                <Dialog.Overlay className="fixed inset-0 bg-black opacity-30" />
-                <div className="fixed inset-0 flex items-center justify-center">
-                    <div className="bg-white rounded-lg shadow-lg p-4">
-                        <Dialog.Title as="h3" className="text-lg font-medium leading-6 text-gray-900">
-                            Warning
-                        </Dialog.Title>
-                        <Dialog.Description className="mt-2 text-sm text-gray-500">
-                            {message}
-                        </Dialog.Description>
-                        <div className="mt-4 sm:mt-6 sm:grid sm:grid-cols-2 sm:gap-3 sm:grid-flow-row-dense">
-                            <button
-                                type="button"
-                                className="inline-flex justify-center px-4 py-2 text-sm font-medium text-white bg-indigo-500 border border-transparent rounded-md hover:bg-gray-300 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-indigo-500"
-                                onClick={() => setIsShown(false)}
-                            >
-                                {t('cancel')}
-                            </button>
-                            <button
-                                type="button"
-                                className="inline-flex justify-center px-4 py-2 text-sm font-medium text-white bg-red-600 border border-transparent rounded-md hover:bg-red-700 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-red-500"
-                                onClick={() => {
-                                    setIsShown(false);
-                                    action()
-                                }}
-                            >
-                                {t('continue')}
-
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </Dialog>      
-        );
-    } else {
-        return <></>;
-    }
-
-}
+const WarningModal: FC<WarningModalProps> = ({ message, isShown, setIsShown, action }) => {
+  const { t } = useTranslation();
+  if (isShown) {
+    return (
+      <Dialog open={isShown} onClose={() => {}}>
+        <Dialog.Overlay className="fixed inset-0 bg-black opacity-30" />
+        <div className="fixed inset-0 flex items-center justify-center">
+          <div className="bg-white rounded-lg shadow-lg p-4">
+            <Dialog.Title as="h3" className="text-lg font-medium leading-6 text-gray-900">
+              Warning
+            </Dialog.Title>
+            <Dialog.Description className="mt-2 text-sm text-gray-500">
+              {message}
+            </Dialog.Description>
+            <div className="mt-4 sm:mt-6 sm:grid sm:grid-cols-2 sm:gap-3 sm:grid-flow-row-dense">
+              <button
+                type="button"
+                className="inline-flex justify-center px-4 py-2 text-sm font-medium text-white bg-indigo-500 border border-transparent rounded-md hover:bg-gray-300 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-indigo-500"
+                onClick={() => setIsShown(false)}>
+                {t('cancel')}
+              </button>
+              <button
+                type="button"
+                className="inline-flex justify-center px-4 py-2 text-sm font-medium text-white bg-red-600 border border-transparent rounded-md hover:bg-red-700 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-red-500"
+                onClick={() => {
+                  setIsShown(false);
+                  action();
+                }}>
+                {t('continue')}
+              </button>
+            </div>
+          </div>
+        </div>
+      </Dialog>
+    );
+  } else {
+    return <></>;
+  }
+};
 
 export default WarningModal;
