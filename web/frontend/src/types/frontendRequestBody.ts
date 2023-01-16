b057a03ff824fe3be92c7e77c8cf894c3818fd95
Ghita Tagemouati <ghita.tagemouati@epfl.ch>
2023-01-16 22:23:53 +0100

    isAu

--- web/frontend/src/types/frontendRequestBody.ts
+++ web/frontend/src/types/frontendRequestBody.ts
@@ -9,6 +9,9 @@ interface NewFormBody {
 interface NewFormVoteBody {
   Ballot: [];
 }
+interface AddAuthBody {
+  FormID: ID;
+}
 
 interface EditFormBody {
   Action: Action.Open | Action.Close | Action.CombineShares | Action.Cancel;
@@ -52,4 +55,5 @@ export type {
   UpdateProxyAddress,
   NewUserRole,
   RemoveUserRole,
+  AddAuthBody,
 };
