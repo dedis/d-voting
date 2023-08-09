250b908e792e59152bdb82cb18e133f11f59c56f
Ahmed Elghareeb <me@rancho.me>
2023-08-09 17:04:58 +0200

    chore: replaces airbnb config with minimal recommendations
    I looked up the latest version of eslint configs. I didn't follow the airbnb ones as they're not suitable for a very small project like ours. 
    
    For the .eslintignore, dist directory appears to be inside the node_modules, so ignoring the nodemodules should be enough

---
+++ web/backend/.eslintrc.cjs
@@ -0,0 +1,32 @@
+module.exports = {
+    'env': {
+        'es2023': true,
+        'node': true
+    },
+    'extends': [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier'
+    ],
+    'overrides': [
+        {
+            'files': [
+                '.eslintrc.{js,cjs}'
+            ],
+        }
+    ],
+    'parser': '@typescript-eslint/parser',
+    "parserOptions": {
+        'ecmaVersion': 'latest',
+        'sourceType': 'script',
+        "project": ["./tsconfig.json"]
+    },
+    'plugins': ['@typescript-eslint', 'prettier'],
+    'rules': {
+        'indent': ['error', 2],
+        "prettier/prettier": "error",
+        'linebreak-style': ['error', 'unix'],
+        'quotes': ['error', 'single'],
+        'semi': ['error', 'always']
+    }
+};
