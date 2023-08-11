e3062c1cf927e6177e69e3c4bbfb394b8c79e96a
Carine Dengler <git@carine-dengler.de>
2023-08-11 11:01:50 +0200

    fix: fix wrong access rights

--- init_dela.sh
+++ init_dela.sh
@@ -21,23 +21,28 @@ done
 docker compose exec dela-leader memcoin --config /data/node ordering setup $MEMBERS;
 
 # authorize the signer to handle the access contract on each node
-IDENTITY=$(docker compose exec dela-leader crypto bls signer read --path /data/private.key --format BASE64_PUBKEY);
-for container in dela-leader dela-worker-1 dela-worker-2; do
-  docker compose exec "$container" memcoin --config /data/node access add --identity "$IDENTITY";
+for signer in dela-leader dela-worker-1 dela-worker-2; do
+  IDENTITY=$(docker compose exec "$signer" crypto bls signer read --path /data/private.key --format BASE64_PUBKEY);
+  for node in dela-leader dela-worker-1 dela-worker-2; do
+    docker compose exec "$node" memcoin --config /data/node access add --identity "$IDENTITY";
+  done
 done
 
 # update the access contract
-docker compose exec dela-leader memcoin --config /data/node pool add\
-    --key /data/private.key\
-    --args go.dedis.ch/dela.ContractArg\
-    --args go.dedis.ch/dela.Access\
-    --args access:grant_id\
-    --args 0200000000000000000000000000000000000000000000000000000000000000\
-    --args access:grant_contract\
-    --args go.dedis.ch/dela.Value\
-    --args access:grant_command\
-    --args all\
-    --args access:identity\
-    --args $IDENTITY\
-    --args access:command\
-    --args GRANT
+for container in dela-leader dela-worker-1 dela-worker-2; do
+  IDENTITY=$(docker compose exec "$container" crypto bls signer read --path /data/private.key --format BASE64_PUBKEY);
+  docker compose exec dela-leader memcoin --config /data/node pool add\
+      --key /data/private.key\
+      --args go.dedis.ch/dela.ContractArg\
+      --args go.dedis.ch/dela.Access\
+      --args access:grant_id\
+      --args 0300000000000000000000000000000000000000000000000000000000000000\
+      --args access:grant_contract\
+      --args go.dedis.ch/dela.Evoting \
+      --args access:grant_command\
+      --args all\
+      --args access:identity\
+      --args $IDENTITY\
+      --args access:command\
+      --args GRANT
+done
