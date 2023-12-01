7cb3d629bf3838c761f6f74e864b0f1c0b585751
Carine Dengler <git@carine-dengler.de>
2023-12-01 17:07:29 +0100

    test: add login/logout utils

---
+++ web/frontend/tests/mocks.ts
@@ -0,0 +1,26 @@
+export const SCIPER_ADMIN = process.env.REACT_APP_SCIPER_ADMIN;
+export const SCIPER_USER = '789012';
+
+export async function mockGetDevLogin (page: any) {
+  await page.routeFromHAR(
+    `./tests/hars/get_dev_login.${SCIPER_ADMIN}.har`,
+    {
+      url: `${process.env.FRONT_END_URL}/api/get_dev_login/${SCIPER_ADMIN}`,
+      update: false,
+    });
+  await page.routeFromHAR(
+    `./tests/hars/get_dev_login.${SCIPER_USER}.har`,
+    {
+      url: `${process.env.FRONT_END_URL}/api/get_dev_login/${SCIPER_USER}`,
+      update: false,
+    });
+}
+
+export async function mockLogout (page: any) {
+  await page.routeFromHAR(
+    './tests/hars/logout.har',
+    {
+      url: `${process.env.FRONT_END_URL}/api/logout`,
+      update: false,
+    });
+}
--- web/frontend/tests/shared.ts
+++ web/frontend/tests/shared.ts
@@ -1,10 +1,25 @@
 import en from './../src/language/en.json';
 import fr from './../src/language/fr.json';
 import de from './../src/language/de.json';
+import { SCIPER_ADMIN, SCIPER_USER, mockGetDevLogin, mockLogout } from './mocks';
 
 export function initI18n () {
   i18n.init({
     resources: { en, fr, de },
     fallbackLng: ['en', 'fr', 'de'],
   });
+}
+
+export async function logIn (page: any, admin = false) {
+  await mockGetDevLogin(page);
+  await page.context().request.get(
+    `${process.env.FRONT_END_URL}/api/get_dev_login/${admin ? SCIPER_ADMIN : SCIPER_USER}`
+  );
+  await page.reload();
+}
+
+export async function logOut (page: any) {
+  await mockLogout(page);
+  await page.context().request.post(`${process.env.FRONT_END_URL}/api/logout`);
+  await page.reload();
 }
