b50a584c73ea949c0fc3b2176ac9e8da000961aa
Carine Dengler <git@carine-dengler.de>
2023-12-07 10:08:51 +0100

    test: clean up authentication mocks

--- web/frontend/tests/mocks.ts
+++ web/frontend/tests/mocks.ts
@@ -1,42 +1,29 @@
-export const SCIPER_ADMIN = '123456';
-export const SCIPER_USER = '789012';
-
-export async function mockPersonalInfo (page: any, authenticated = false, admin = false) {
-  if (!authenticated) {
-    await page.routeFromHAR(
-      './tests/hars/personal_info.har',
-      {
-        url: `${process.env.FRONT_END_URL}/api/personal_info`,
-        update: false,
-      });
-      return;
-  }
+export async function mockPersonalInfo (page: any, sciper) {
   await page.routeFromHAR(
-    `./tests/hars/personal_info.${admin ? SCIPER_ADMIN : SCIPER_USER}.har`,
+    sciper ? `./tests/hars/personal_info.${sciper}.har` : './tests/hars/personal_info.har',
     {
       url: `${process.env.FRONT_END_URL}/api/personal_info`,
       update: false,
     });
 }
 
-export async function mockGetDevLogin (page: any) {
+export async function mockGetDevLogin (page: any, sciper) {
   await page.routeFromHAR(
-    `./tests/hars/get_dev_login.${SCIPER_ADMIN}.har`,
+    `./tests/hars/get_dev_login.${sciper}.har`,
     {
-      url: `${process.env.FRONT_END_URL}/api/get_dev_login/${SCIPER_ADMIN}`,
+      url: `${process.env.FRONT_END_URL}/api/get_dev_login/${sciper}`,
       update: false,
     });
-  await page.routeFromHAR(
-    `./tests/hars/get_dev_login.${SCIPER_USER}.har`,
-    {
-      url: `${process.env.FRONT_END_URL}/api/get_dev_login/${SCIPER_USER}`,
-      update: false,
-    });
+  // dummy route for "Login" button depending on local configuration
+  await page.route(
+    `${process.env.FRONT_END_URL}/api/get_dev_login/${process.env.REACT_APP_SCIPER_ADMIN}`,
+    async route => {await route.fulfill({});}
+  );
 }
 
-export async function mockLogout (page: any) {
+export async function mockLogout (page: any, sciper) {
   await page.routeFromHAR(
-    './tests/hars/logout.har',
+    `./tests/hars/logout.${sciper}.har`,
     {
       url: `${process.env.FRONT_END_URL}/api/logout`,
       update: false,
--- web/frontend/tests/shared.ts
+++ web/frontend/tests/shared.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 import en from './../src/language/en.json';
 import fr from './../src/language/fr.json';
 import de from './../src/language/de.json';
-import { SCIPER_ADMIN, SCIPER_USER, mockPersonalInfo, mockGetDevLogin, mockLogout } from './mocks';
+import { mockPersonalInfo, mockGetDevLogin, mockLogout } from './mocks';
 
 export function initI18n () {
   i18n.init({
@@ -12,27 +12,38 @@ export function initI18n () {
   });
 }
 
-export async function logIn (page: any, admin = false) {
-  await mockPersonalInfo(page, true, admin);
+export const SCIPER_ADMIN = '123456';
+export const SCIPER_USER = '789012';
+
+export async function logIn (page: any, sciper) {
+  await mockGetDevLogin(page, sciper);
+  await mockPersonalInfo(page, sciper);
+  // uncomment the following line to update the HAR files
+  await page.context().request.get(`${process.env.FRONT_END_URL}/api/get_dev_login/${sciper}`);
   await page.reload();
+  await expect(page).toHaveURL(page.url());   // make sure that page is loaded correctly
 }
 
-export async function logOut (page: any) {
-  await mockPersonalInfo(page)
+export async function logOut (page: any, sciper) {
+  await mockLogout(page, sciper);
+//  await mockPersonalInfo(page, null);
+  // uncomment the following line to update the HAR files
+  await page.context().request.post(`${process.env.FRONT_END_URL}/api/logout`);
   await page.reload();
+  await expect(page).toHaveURL(page.url());   // make sure that page is loaded correctly
 }
 
 export async function assertOnlyVisibleToAuthenticated (page: any, locator: any) {
   await expect(locator).toBeHidden();   // assert is hidden to unauthenticated user
-  await logIn(page);
+  await logIn(page, SCIPER_USER);
   await expect(locator).toBeVisible();  // assert is visible to authenticated user
 }
 
 export async function assertOnlyVisibleToAdmin (page: any, locator: any) {
   await expect(locator).toBeHidden();     // assert is hidden to unauthenticated user
-  await logIn(page);
+  await logIn(page, SCIPER_USER);
   await expect(locator).toBeHidden();     // assert is hidden to authenticated non-admin user
   await logOut(page);
-  await logIn(page, true);
+  await logIn(page, SCIPER_ADMIN);
   await expect(locator).toBeVisible();    // assert is visible to admin user
 }
