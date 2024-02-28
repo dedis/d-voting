9fab6490081b8bd89ee5a1c0d2096bf83e86ce49
Linus Gasser <linus.gasser@epfl.ch>
2024-02-28 10:12:13 +0100

    Add voting script to the backend
    This also updates some of the outdated settings in the frontend

---
+++ web/backend/run_votes.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [[ "$1" == "" ]]; then
+  echo "please run $0 with form-ID"
+  exit 1
+fi
+
+votes=${2:-120}
+
+cls
+ts-node src/cli.ts vote -f https://dvoting.c4dt.org \
+	-e $1 \
+	-b $votes -a 111111
