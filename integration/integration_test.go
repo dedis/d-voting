bd6ec9eb506d5a7aa802426cee0fe72fb36b660d
Maxime Zammit <maxime.zammit@epfl.ch>
2024-04-22 14:51:15 +0200

    patch dummy userid to sciper format part 2

--- integration/integration_test.go
+++ integration/integration_test.go
@@ -174,7 +174,8 @@ func getIntegrationTestCrash(numNodes, numVotes, failingNodes int) func(*testing
 	return func(t *testing.T) {
 		t.Parallel()
 
-		adminID := "first admin"
+		// Dummy UserID in SCIPER format
+		adminID := "123456"
 
 		// ##### SETUP ENV #####
 
@@ -322,7 +323,8 @@ func getIntegrationTestCrash(numNodes, numVotes, failingNodes int) func(*testing
 func getIntegrationBenchmark(numNodes, numVotes int) func(*testing.B) {
 	return func(b *testing.B) {
 
-		adminID := "first admin"
+		// Dummy UserID in SCIPER format
+		adminID := "123456"
 
 		// ##### SETUP ENV #####
 
